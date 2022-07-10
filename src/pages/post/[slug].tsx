8f1c95375f7ef3c1f315ca25e5e7a6a562a9b65e
diego.estanislau <diego.estanislau@jbq.global>
2022-07-10 14:59:23 -0300

    :tada: deploy

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,15 @@
+import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiUser, FiCalendar, FiClock } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Header from '../../components/Header';
 
 interface Post {
   first_publication_date: string | null;
@@ -11,6 +17,7 @@ interface Post {
     title: string;
     banner: {
       url: string;
+      alt: string;
     };
     author: string;
     content: {
@@ -26,20 +33,106 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+interface FormatReadingTime {
+  heading: string;
+  body: {
+    text: string;
+  }[];
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
 
-//   // TODO
-// };
+  if (router.isFallback) {
+    return <p>Carregando...</p>;
+  }
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  const estimatedReadingTime = (contents: FormatReadingTime[]): number => {
+    const totalLenght = contents.reduce((accumulator, content) => {
+      const bodyLength = content.body.reduce((bodyAccumulator, body) => {
+        const newBodyAccumulator = bodyAccumulator + body.text.length;
 
-//   // TODO
-// };
+        return newBodyAccumulator;
+      }, 0);
+
+      const headingLength = content.heading.length;
+
+      const newAccumulator = accumulator + bodyLength + headingLength;
+
+      return newAccumulator;
+    }, 0);
+
+    return Math.ceil(totalLenght / 200);
+  };
+
+  return (
+    <>
+      <Header />
+      <section className={styles.bannerContainer}>
+        <img src={post.data.banner.url} alt={post.data.banner.alt} />
+      </section>
+      <main className={commonStyles.container}>
+        <h1 className={styles.title}>{post.data.title}</h1>
+        <section className={styles.metaData}>
+          <div>
+            <FiCalendar />
+            <span>
+              {format(new Date(post.first_publication_date), 'd MMM yyy', {
+                locale: ptBR,
+              })}
+            </span>
+          </div>
+          <div>
+            <FiUser />
+            <span>{post.data.author}</span>
+          </div>
+          <div>
+            <FiClock />
+            <span>4 min</span>
+          </div>
+        </section>
+
+        <section className={styles.content}>
+          {post.data.content.map(content => (
+            <div className={styles.postSection} key={content.heading}>
+              <h1>{content.heading}</h1>
+              {content.body.map(bodyContent => (
+                <p key={bodyContent.text}>{bodyContent.text}</p>
+              ))}
+            </div>
+          ))}
+        </section>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.getByType('post', {
+    pageSize: 1,
+  });
+
+  const paths = posts.results.map(post => ({
+    params: {
+      slug: post.uid,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('post', slug as string, {});
+  return {
+    props: {
+      post: response,
+    },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,9 @@
-import * as prismic from '@prismicio/client';
-import { HttpRequestLike } from '@prismicio/client';
-import { enableAutoPreviews } from '@prismicio/next';
+import * as Prismic from '@prismicio/client';
 
-export interface PrismicConfig {
-  req?: HttpRequestLike;
-}
+export function getPrismicClient(req?: unknown) {
+  const prismic = Prismic.createClient(process.env.PRISMIC_API_ENDPOINT, {
+    req,
+  });
 
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
-
-  enableAutoPreviews({
-    client,
-    req: config.req,
-  })
-
-  return client;
+  return prismic;
 }
